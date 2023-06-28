5e95c6fff26839d7fbe698ebe6befe0b9670ccdf
Pritesh Zaver <priteshzaver@gmail.com>
2023-06-28 12:37:55 -0500

    initial commit

---
+++ src/views/CommunityView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>CommunityView</div>
+</template>
+
+<style scoped></style>
---
+++ src/views/CreateMadlibsView.vue
@@ -0,0 +1,7 @@
+<script setup></script>
+
+<template>
+  <div>CreateMadlibsView</div>
+</template>
+
+<style scoped></style>
